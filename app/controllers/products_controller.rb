f49e70f09a26373433cbff668216bc49f22045ff
Tin Trung Dang <tindang1710@gmail.com>
2017-06-11 18:46:47 -0700

    Display customers' review and average rating

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,6 +6,8 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.find params[:id]
+    @review = @product.reviews
+    @new_review= Review.new
   end
 
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,28 @@
 class ReviewsController < ApplicationController
+  
+  before_filter :authorize
+
+  
   def create
+    @review = Review.new(review_params)
+    @review.product_id = params[:product_id]
+    @review.user = current_user
+    
+    if @review.save
+      redirect_to @review.product, notice: 'Review was successfully created.'
+    else
+      redirect_to @review.product, notice: 'Failed to create the review'
+    end
+  end
+  
+  def destroy
+    @review = Review.find params[:id]
+    @review.destroy
+    redirect_to :back, notice: 'Product deleted!'
+  end
+
+  private
+  def review_params
+    params.require(:review).permit(:rating, :description)
   end
 end
