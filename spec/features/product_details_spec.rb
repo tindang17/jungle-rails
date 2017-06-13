e9f97e2e3bdeb433732919cf29a18b050706aa0e
Tin Trung Dang <tindang1710@gmail.com>
2017-06-13 14:01:54 -0700

    tested product detail

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
-RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
-
+RSpec.feature "ProductDetails", type: :feature, js: true do
   # SETUP
   before :each do
     @category = Category.create! name: 'Apparel'
@@ -16,15 +15,17 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
       )
     end
   end
-
-  scenario "They see all products" do
+  scenario "They see product details" do
     # ACT
     visit root_path
-
-    # DEBUG
-    save_screenshot
-
+    
+    first('article.product').find_link('Details').click
+    
+    # sleep 2
     # VERIFY
-    expect(page).to have_css 'article.product', count: 10
+    expect(page).to have_css 'article.product-detail'
+    # DEBUG
+    save_screenshot 'product-detail.png'
+
   end
 end
